0fc0ff478ba3cde06c79ea90b18197471b84a4e4
ArianTrovato <fedetrovato21@gmail.com>
2024-11-18 20:18:15 -0300

    new module Projects

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  
   getInfo(): string {
     return 'Welcome to my portfolio API, built with NestJS! This project showcases my skills, projects, and experience in software development. Explore the endpoints to learn more about my journey and work.';
   }
